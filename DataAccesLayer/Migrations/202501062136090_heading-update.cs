abb79cef6b52e66105d7965187820933078add78
AzmiOzmn <azmiozmen5234@gmail.com>
2025-01-07 00:45:09 +0300

    Add project files.

---
+++ DataAccesLayer/Migrations/202501062136090_heading-update.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccesLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class headingupdate : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Headings", "Heading_HeadingId", "dbo.Headings");
+            DropIndex("dbo.Headings", new[] { "Heading_HeadingId" });
+            DropColumn("dbo.Headings", "Heading_HeadingId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Headings", "Heading_HeadingId", c => c.Int());
+            CreateIndex("dbo.Headings", "Heading_HeadingId");
+            AddForeignKey("dbo.Headings", "Heading_HeadingId", "dbo.Headings", "HeadingId");
+        }
+    }
+}
